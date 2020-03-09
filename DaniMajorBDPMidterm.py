66926b50390d2439d74b55a2cf1dface1db731fa
danijmajor <41305110+danijmajor@users.noreply.github.com>
2020-03-08 23:26:25 -0400

    Update DaniMajorBDPMidterm.py

--- DaniMajorBDPMidterm.py
+++ DaniMajorBDPMidterm.py
@@ -1,25 +1,21 @@
 from pyspark import SparkConf, SparkContext
+
+
 import aspell
 
-conf = SparkConf().setMaster('local').setAppName('DMajorBDPMidterm.py')
+conf = SparkConf().setMaster('local').setAppName('DMajorMidterm.py')
 sc = SparkContext(conf = conf)
 
 #import files
 fileToAnalyze1 = sc.textFile('/home/dani/Downloads/Encrypted-1.txt')
-fileToAnalyze1.take(1)
-
 fileToAnalyze2 = sc.textFile('/home/dani/Downloads/Encrypted-2.txt')
-fileToAnalyze2.take(1)
-
 fileToAnalyze3 = sc.textFile('/home/dani/Downloads/Encrypted-3.txt')
-fileToAnalyze3.take(1)
-
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def get_words(name):
     #separate words
     global words
     words = name.map(lambda line: str(line)).flatMap(lambda line: line.split())
-    words.take(1)
     #get total number of words
     words_count = words.map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y)
     total_words = sum(words_count.collectAsMap().values())
@@ -34,7 +30,6 @@ def get_chars(name):
     #separate characters
     global chars
     chars = name.flatMap(lambda line: line)
-    chars.take(5)
     #get total number of chars
     char_count = chars.map(lambda char: (char, 1)).reduceByKey(lambda x, y: x + y)
     chars_counts = char_count.collectAsMap()
@@ -57,8 +52,6 @@ def get_chars(name):
     print("The character count for this document is : " + str(total_chars))
     print("The most common character frequency for this file is : " + top_char)
 
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 
 def get_shift(s):
     global shift
@@ -69,14 +62,13 @@ def get_shift(s):
 
 
 def decrypt(ss, shift):
-    return "".join([
-        letters[(letters.find(s) - shift) % len(letters)]
+    return "".join([letters[(letters.find(s) - shift)]
         if s in letters else s
-        for s in ss
-    ])
+        for s in ss])
 
 
 #initalize speller for each partition
+#speller not serrializable so we have to do it by partition
 def check_words(words):
     a = aspell.Speller('lang', 'en')
     for word in words:
@@ -86,7 +78,7 @@ def check_words(words):
 def translate(name, path):
     decrypted = words.map(lambda w: decrypt(w, shift=shift))
     #find the fraction of decrypted words that are in the dictionary
-    decrypted.map(check_words).mean()
+    decrypted.mapPartitions(check_words).mean()
     #save as text file
     name.coalesce(1).map(lambda line: decrypt(line, shift)).saveAsTextFile(path)
 
@@ -98,6 +90,6 @@ def output(name, path):
     translate(name, path)
     print(" ")
 
-output(fileToAnalyze1,"/tmp/output1/")
-output(fileToAnalyze2,"/tmp/output2/")
-output(fileToAnalyze3,"/tmp/output3/")
+output(fileToAnalyze1,"/tmp/01/")
+output(fileToAnalyze2,"/tmp/2/")
+output(fileToAnalyze3,"/tmp/3/")
